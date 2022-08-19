f69fa75ea1de7a84295d30b7283f154567992ed3
zerop <zerop.development@gmail.com>
2022-08-19 03:49:56 +0300

    add two sort function and swap function. fix

--- src/sort/bubble-sort.ts
+++ src/sort/bubble-sort.ts
@@ -1,5 +1,4 @@
 function bubleSort (list: Array<number>): Array<number> {
-
     for (let i = 0; i < list.length; i++) {
         for (let j = 0; j < list.length; j++) {
             if (list[j] > list[j + 1]) {
